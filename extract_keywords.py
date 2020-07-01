695b9b803d77f7a2cc12f4e5eb66057edfb59f38
Deepak Kandel <deepak.kandel@docsumo.com>
2020-07-01 22:55:16 +0545

    Refactored the code

--- extract_keywords.py
+++ extract_keywords.py
@@ -3,7 +3,7 @@ import subprocess
 from string import punctuation
 
 
-def extract_keywords(nlp, sequence, special_tags : list = None):
+def extract_keywords(nlp, sequence, special_tags: list = None):
     """Takes a Spacy core language model,
     string sequence of text and optional
     list of special tags as arguements.
@@ -23,15 +23,15 @@ def extract_keywords(nlp, sequence, special_tags : list = None):
     """
     result = []
 
-    #custom list of part of speech tags we are interested in
+    # custom list of part of speech tags we are interested in
     # we are interested in proper nouns, nouns, ans adjectives
     # edit this list of POS tags according to your needs.
-    pos_tag = ['PROPN','NOUN','ADJ'] 
+    pos_tag = ["PROPN", "NOUN", "ADJ"]
 
-    #create a spacy doc object by calling the nlp object on the input sequence
-    doc = nlp(sequence.lower()) 
+    # create a spacy doc object by calling the nlp object on the input sequence
+    doc = nlp(sequence.lower())
 
-    #if special tags are given and exits in the input sequence
+    # if special tags are given and exits in the input sequence
     # add them to results by default
 
     if special_tags:
@@ -43,30 +43,34 @@ def extract_keywords(nlp, sequence, special_tags : list = None):
     for chunk in doc.noun_chunks:
         final_chunk = ""
         for token in chunk:
-            if (token.pos_ in pos_tag):
-                final_chunk = final_chunk + token.text + " " 
+            if token.pos_ in pos_tag:
+                final_chunk = final_chunk + token.text + " "
         if final_chunk:
             result.append(final_chunk.strip())
 
-
     for token in doc:
-        if (token.text in token.text in punctuation):
+        if token.text in token.text in punctuation:
             continue
-        if (token.pos_ in pos_tag):
+        if token.pos_ in pos_tag:
             result.append(token.text)
     return list(set(result))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """install the language model using the subprocess package
     useful whrn hosting the service in the cloud as it prevents against
     us forgetting to do this via the CLI
     """
-    subprocess.call("python -m spacy download en_core_web_sm",shell=True)
-    nlp = spacy.load('en_core_web_sm')
+    subprocess.call("python -m spacy download en_core_web_sm", shell=True)
+    nlp = spacy.load("en_core_web_sm")
 
     print("Loaded language vacabulary")
 
-    print(extract_keywords(nlp,"""Learninig how to use Natural Language Processing
+    print(
+        extract_keywords(
+            nlp,
+            """Learninig how to use Natural Language Processing
                                 in Python and build Flask's API is easy when you have
-                                packages like spacy anc fuzzywuzzy"""))
+                                packages like spacy anc fuzzywuzzy""",
+        )
+    )
--- fuzzy.py
+++ fuzzy.py
@@ -1,7 +1,8 @@
 from fuzzywuzzy import process
 from fuzzywuzzy.fuzz import ratio
 
-def get_fuzzy_similarity(token = None, dictionary = None):
+
+def get_fuzzy_similarity(token=None, dictionary=None):
     """Returns similar words ans similary scores for a given token
     from a provided dictonary of words.
 
@@ -14,15 +15,16 @@ def get_fuzzy_similarity(token = None, dictionary = None):
         [list] -- a list of tuples in the form '(matched_word, similarity score)'
     """
 
-
     if token in dictionary:
-        return process.extractBests(token, dictionary,scorer=ratio,score_cutoff=70)
+        return process.extractBests(token, dictionary, scorer=ratio, score_cutoff=70)
     else:
         return []
-if __name__ == '__main__':
-    #toy doctionary of words will be query our mispelt input against   
-    word_dictionary = ['medium','Twitter','Google','LiknedIn','Facebook']
 
-    #print the result of the fuzzy match search 
 
-    print(get_fuzzy_similarity('mediaum',word_dictionary))
+if __name__ == "__main__":
+    # toy doctionary of words will be query our mispelt input against
+    word_dictionary = ["medium", "Twitter", "Google", "LiknedIn", "Facebook"]
+
+    # print the result of the fuzzy match search
+
+    print(get_fuzzy_similarity("mediaum", word_dictionary))
